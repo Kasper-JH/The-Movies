321f914cf4335e64b4138ae47698d503fc8d2a1a
Daniel <DGARosendal@gmail.com>
2026-09-01 23:21:32 +0200

    Removed persistence of number of halls that a cinema has in .json file (redundancy)

--- TheMovies/Core/Models/Cinema.cs
+++ TheMovies/Core/Models/Cinema.cs
@@ -4,6 +4,7 @@
  */
 
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace TheMovies.Core.Models
 {
@@ -19,6 +20,11 @@ namespace TheMovies.Core.Models
         }
 
         public string Name { get; set; }
+        // Vi ønsker ikke at persiste antallet af tilgængelige sale en biograf har i json-filen
+        // (InFile), da dette er spild af plads (redundant). Dette fordi denne data ligger klar
+        // InMemory i CinemaSeed.cs. Derfor markeres Halls med [JsonIgnore] så den ikke bliver
+        // serialiseret til json-filen.
+        [JsonIgnore]
         public List<Hall> Halls { get; set; } = new List<Hall>();
 
         // Tilføjer en sal til biografen (bruges kun ved opsætning af faste data)
