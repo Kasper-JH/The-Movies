c8f1875570d7cdbd1d92a38bad0ebd69097029bb
Michelle <michellebruun97@gmail.com>
2026-09-03 12:27:23 +0200

    Unittest project setup, tom test

---
+++ TheMoviesUnittest/Tests.cs
@@ -0,0 +1,26 @@
+﻿using TheMovies;
+
+namespace TheMoviesUnittest
+
+{
+    [TestClass]
+    public sealed class Tests
+    {
+        [TestInitialize]
+        public void SetupForTest()
+        {
+            //init hvis nødvendigt for tests
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
